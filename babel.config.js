728b35bd3970e3122491b4ffedc4517667cb2969
Zain Sajjad <zsajjad93@gmail.com>
2021-04-13 15:42:20 +0500

    feat: add TS support

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./app'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          tests: ['./tests/'],
+        },
+      },
+    ],
+  ],
+  env: {
+    production: {
+      plugins: ['transform-remove-console'],
+    },
+  },
 };

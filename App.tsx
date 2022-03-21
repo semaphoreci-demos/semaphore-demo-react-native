fe1c75bfbecae527386c059e5e111595de0d4d40
Katharine Padilha <katharinepadilha@gmail.com>
2022-03-21 08:19:02 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -5,13 +5,13 @@
  */
 
 import React from 'react';
-import {StatusBar} from 'react-native';
+import { StatusBar } from 'react-native';
 // import {enableScreens} from 'react-native-screens';
 
 import Router from './app/router';
 
 // enableScreens();
-
+//test
 const App: React.FC = () => {
   return (
     <>
